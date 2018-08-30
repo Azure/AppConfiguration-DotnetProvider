a6a1ebdb12e6b6334d395cc0087ec4b2600b89e8
Jimmy Campbell <jimmyca@microsoft.com>
2018-08-30 09:56:03 -0700

    Updated console example to be more approachable for quickstart scenarios.

--- examples/Application/Program.cs
+++ examples/Application/Program.cs
@@ -3,6 +3,7 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Configuration.Azconfig;
     using System;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -30,43 +31,60 @@
             var builder = new ConfigurationBuilder();
 
             // load some local configurations from files
-            builder.AddJsonFile("appsettings.json");
+            builder.AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
 
             IConfiguration configuration = builder.Build();
 
-            // load key-values with prefix "App" label "label1" and listen two keys.
+            if (string.IsNullOrEmpty(configuration["AzconfigConnectionString"]))
+            {
+                Console.WriteLine("Connection string not found.");
+                Console.WriteLine("Please set the AzconfigConnectionString environment variable to a valid Azconfig connection string to and re-run this example.");
+                return;
+            }
+
+            //
             // Pull configuration connection string from environment variable
             builder.AddRemoteAppConfiguration(o => {
-                o.Connect(configuration["connection_string"])
-                 .Use("App*", "label1")
-                 .Watch("Language", 1000, "label1")
-                 .Watch("AppName", 1000, "label1");
+
+                o.Connect(configuration["AzconfigConnectionString"])
+                 //
+                 // Uncomment to filter selected key-values by key and/or label
+                 //.Use("App*", "label1")
+                 .Watch("Language", 1000)
+                 .Watch("AppName", 1000);
             });
+
             Configuration = builder.Build();
         }
 
         private static async Task Run(CancellationToken token)
         {
+            string display = string.Empty;
+            StringBuilder sb = new StringBuilder();
+
             while (!token.IsCancellationRequested)
             {
-                Console.Clear();
+                sb.AppendLine($"{Configuration["AppName"]} has been configured to run in {Configuration["Language"]}");
+                sb.AppendLine();
 
-                Console.WriteLine("You're running " + Configuration["AppName"]);
-                Console.WriteLine();
+                sb.AppendLine(string.Equals(Configuration["Language"], "spanish", StringComparison.OrdinalIgnoreCase) ? "Buenos Dias." : "Good morning");
+                sb.AppendLine();
 
-                Console.WriteLine(string.Equals(Configuration["Language"], "spanish", StringComparison.OrdinalIgnoreCase) ? "Buenos Dias." : "Good morning");
-                Console.WriteLine();
-
-                foreach (var section in Configuration.GetChildren())
-                {
-                    Console.WriteLine($"{section.Key}: {section.Value}");
-                }
-
-                Console.WriteLine();
-
-                Console.WriteLine("Press any key to exit...");
+                sb.AppendLine("Press any key to exit...");
 
                 await Task.Delay(1000);
+
+                if (!sb.ToString().Equals(display))
+                {
+                    display = sb.ToString();
+
+                    Console.Clear();
+
+                    Console.Write(display);
+                }
+
+                sb.Clear();
             }
         }
     }
