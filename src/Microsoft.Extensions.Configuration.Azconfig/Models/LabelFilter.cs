996833cb7723076615ee1c5cee65464d6d8d6ae6
Yiming Jia <yijia@microsoft.com>
2019-01-25 11:57:57 -0800

    Improve comment

--- src/Microsoft.Extensions.Configuration.Azconfig/Models/LabelFilter.cs
+++ src/Microsoft.Extensions.Configuration.Azconfig/Models/LabelFilter.cs
@@ -3,7 +3,7 @@
     public class LabelFilter
     {
         /// <summary>
-        /// The label filter used to query all key-values with empty label.
+        /// The label filter used to filter queried key-values with empty label.
         /// </summary>
         const string Empty = "\0";
     }
