0a4e49f72d803c777a6c6990d66608c65fca15f1
Yiming Jia <yijia@microsoft.com>
2019-01-25 16:09:03 -0800

    Add KeyFilter LabelFilter
    * Add KeyFilter and LabelFilter

---
+++ src/Microsoft.Extensions.Configuration.Azconfig/Models/KeyFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Extensions.Configuration.Azconfig.Models
+{
+    public class KeyFilter
+    {
+        /// <summary>
+        /// The key filter used to persist all key-vaults.
+        /// </summary>
+        public const string Any = "*";
+    }
+}
---
+++ src/Microsoft.Extensions.Configuration.Azconfig/Models/LabelFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Extensions.Configuration.Azconfig.Models
+{
+    public class LabelFilter
+    {
+
+        /// <summary>
+        /// The label filter used to filter key-values with empty label.
+        /// </summary>
+        public const string Null = "\0";
+    }
+}
