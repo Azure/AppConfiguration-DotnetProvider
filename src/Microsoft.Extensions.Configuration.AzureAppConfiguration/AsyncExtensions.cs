eb5e0c5b6b75f63d3055b8b58a7a6a9fb0d877f2
Vijay Sankar Ganesh <visanka@microsoft.com>
2019-05-24 15:25:33 -0700

    Moved observables to provider

---
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AsyncExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
+{
+    internal static class AsyncExtensions
+    {
+        public static async Task<IEnumerable<T>> ToEnumerableAsync<T>(this IAsyncEnumerable<T> enumerable, CancellationToken cancellationToken)
+        {
+            var iter = enumerable.GetEnumerator();
+            var items = new List<T>();
+
+            while (await iter.MoveNext(cancellationToken))
+            {
+                items.Add(iter.Current);
+            }
+
+            return items;
+        }
+    }
+}
