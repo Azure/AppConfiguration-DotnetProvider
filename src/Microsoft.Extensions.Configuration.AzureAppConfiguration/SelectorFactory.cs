ccb1926a410add353c346b2dd49c52ee8e484a7e
Anne Loomis Thompson <annelo@microsoft.com>
2019-10-23 12:43:37 -0700

    Handle commas in filters correctly.

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/SelectorFactory.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/SelectorFactory.cs
@@ -12,11 +12,26 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 
         public static SettingSelector CreateSettingSelector(string keyFilter, string labelFilter, DateTimeOffset? asOf = default, SettingFields fields = SettingFields.All)
         {
-            return new SettingSelector(keyFilter, labelFilter)
+            // Convert from comma literal format to SDK convention
+            string[] keyFilters = keyFilter?.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+
+            SettingSelector selector = new SettingSelector()
             {
                 AsOf = asOf,
                 Fields = fields
             };
+
+            selector.Keys.Clear();
+            selector.Labels.Clear();
+
+            foreach (var filter in keyFilters)
+            {
+                selector.Keys.Add(filter);
+            }
+
+            selector.Labels.Add(labelFilter);
+
+            return selector;
         }
     }
 }
