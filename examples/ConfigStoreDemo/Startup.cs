cd4b6c0139912681b27e48dd6eccd2f266ef07a3
Abhilash Arora <47916289+abhilasharora@users.noreply.github.com>
2020-07-22 15:04:43 -0700

    Add multi-targeting support for .NET Core 3.1 (#189)
    Co-authored-by: Abhilash Arora <abhilash.arora@microsoft.com>

--- examples/ConfigStoreDemo/Startup.cs
+++ examples/ConfigStoreDemo/Startup.cs
@@ -3,7 +3,9 @@
 //
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.ConfigStoreDemo
 {
@@ -19,17 +21,21 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.Conf
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MvcOptions>(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
+
             services.Configure<Settings>(Configuration.GetSection("Settings"));
             services.AddAzureAppConfiguration();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
