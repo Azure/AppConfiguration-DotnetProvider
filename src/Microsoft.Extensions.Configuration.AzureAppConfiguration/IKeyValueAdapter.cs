01533ee68c128eba4080bc7e240c4d1f6eddcaff
Brandon Robbins <brandonr@nccpa.net>
2021-01-15 17:01:55 -0500

    added documentation

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/IKeyValueAdapter.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/IKeyValueAdapter.cs
@@ -8,10 +8,24 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 {
+    /// <summary>
+    /// Defines the necessary interface to process configuration settings into key value pairs
+    /// </summary>
     public interface IKeyValueAdapter
     {
+        /// <summary>
+        /// Processed settings into key value pairs
+        /// </summary>
+        /// <param name="setting">The <see cref="ConfigurationSetting"/> to be processed</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The processed settings</returns>
         Task<IEnumerable<KeyValuePair<string, string>>> ProcessKeyValue(ConfigurationSetting setting, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Whether the setting can be processed by the adapter
+        /// </summary>
+        /// <param name="setting">The <see cref="ConfigurationSetting"/> in question</param>
+        /// <returns>A flag</returns>
         bool CanProcess(ConfigurationSetting setting);
     }
 }
