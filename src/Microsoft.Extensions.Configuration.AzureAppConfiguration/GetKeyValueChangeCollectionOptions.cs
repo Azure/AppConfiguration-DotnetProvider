c45335f14a5c3acb14a77f79f3dd4d3983fcb395
Avani Gupta <avanigupta@users.noreply.github.com>
2021-03-25 17:22:16 -0700

    Support prefix filtering and prefix trimming from feature flags (#234)

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/GetKeyValueChangeCollectionOptions.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/GetKeyValueChangeCollectionOptions.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 {
     internal class GetKeyValueChangeCollectionOptions
     {
-        public string Prefix { get; set; }
+        public string KeyFilter { get; set; }
         public string Label { get; set; }
         public bool RequestTracingEnabled { get; set; }
         public HostType HostType { get; set; }
