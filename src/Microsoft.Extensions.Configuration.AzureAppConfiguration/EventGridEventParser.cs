2994f32fb768608f8523cbd619bebc03b3ef5adb
MatthewSolomon <matthew.be.solomon@gmail.com>
2021-10-12 11:08:18 -0700

    moved TryParse to new class EventGridEventParser and renamed to TryParseJson. Added testing

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/EventGridEventParser.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/EventGridEventParser.cs
@@ -1,32 +1,32 @@
 using System.Text.Json;
-using System.Reflection;
 using System;
+using System.Collections.Generic;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 {
 	/// <summary>
 	/// Summary for PushNotificationParser
 	/// </summary>
-	public static class PushNotificationParser
+	public static class EventGridEventParser
 	{
 		/// <summary>
 		///  Attempts to parse the message and populate pushNotification
 		/// </summary>
-		/// <param name="message"> Message Data returned from the provider</param>
+		/// <param name="eventGridEvent"> Message Data from Event Grid</param>
 		/// <param name="pushNotification"> out parameter which will try to be populated</param>
 		/// <returns></returns>
-		public static bool TryParse(string message, out PushNotification pushNotification)
+		public static bool TryParseJson(string eventGridEvent, out PushNotification pushNotification)
 		{
 			pushNotification = new PushNotification();
 
-			if (message == null)
+			if (eventGridEvent == null)
 			{
 				return false;
 			}
 
 			try
 			{
-				JsonElement jsonMessage = JsonDocument.Parse(message).RootElement;
+				JsonElement jsonMessage = JsonDocument.Parse(eventGridEvent).RootElement;
 
 				pushNotification.SyncToken = jsonMessage.GetProperty("data").GetProperty("syncToken").GetString();
 				pushNotification.EventType = jsonMessage.GetProperty("eventType").GetString();
@@ -35,10 +35,11 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 				return true;
 			}
 			catch (JsonException) { }
-			catch (AmbiguousMatchException) { }
+			catch (KeyNotFoundException) { }
 			catch (ArgumentException) { }
 			catch (InvalidOperationException) { }
 
+			pushNotification = null;
 			return false;
 		}
 	}
