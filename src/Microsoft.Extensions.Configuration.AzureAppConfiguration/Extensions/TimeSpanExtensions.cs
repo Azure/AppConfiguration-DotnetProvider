e11310251b5a7be45c46fa077ec98dc4b7477867
Avani Gupta <avanigupta@users.noreply.github.com>
2021-10-29 11:44:08 -0700

    Validate attempts

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Extensions/TimeSpanExtensions.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Extensions/TimeSpanExtensions.cs
@@ -14,8 +14,13 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Extensions
         /// <returns>The calculated exponential backoff time.</returns>
         public static TimeSpan CalculateBackoffTime(this TimeSpan maxBackoff, int attempts)
         {
-            long ticks = MinBackoff.Ticks * new Random().Next(0, (int)Math.Min(Math.Pow(2, attempts - 1), int.MaxValue));
-            TimeSpan calculatedBackoff = TimeSpan.FromTicks(Math.Max(MinBackoff.Ticks, ticks));
+            if (attempts < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(attempts), attempts, "The number of attempts should not be less than 1.");
+            }
+
+            long ticks = MinBackoff.Ticks * new Random().Next(1, (int)Math.Min(Math.Pow(2, attempts - 1), int.MaxValue));
+            TimeSpan calculatedBackoff = TimeSpan.FromTicks(ticks);
 
             return TimeSpan.FromTicks(Math.Min(maxBackoff.Ticks, calculatedBackoff.Ticks));
         }
