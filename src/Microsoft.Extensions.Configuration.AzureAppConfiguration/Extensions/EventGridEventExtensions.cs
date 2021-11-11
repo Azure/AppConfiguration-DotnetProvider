e83e295a263ecd76b81242b613a5662e251be4a9
MBSolomon <89044647+MBSolomon@users.noreply.github.com>
2021-11-11 12:43:34 -0800

    strange UI formatting off

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Extensions/EventGridEventExtensions.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Extensions/EventGridEventExtensions.cs
@@ -7,55 +7,55 @@ using System.Text.Json;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Extensions
 {
-	/// <summary>
-	/// This class offers extensions for EventGridEvents.
-	/// </summary>
-	public static class EventGridEventExtensions
+    /// <summary>
+    /// This class offers extensions for EventGridEvents.
+    /// </summary>
+    public static class EventGridEventExtensions
     {
-		private const string SyncTokenPropertyName = "syncToken";
+        private const string SyncTokenPropertyName = "syncToken";
 
-		/// <summary>
-		/// This method uses an EventGridEvent from EventGrid and tries to create a <see cref="PushNotification"/>
-		/// </summary>
-		/// <param name="eventGridEvent"> EventGridEvent from EventGrid</param>
-		/// <param name="pushNotification"> out parameter set up in this method</param>
-		/// <returns></returns>
-		public static bool TryCreatePushNotification(this EventGridEvent eventGridEvent, out PushNotification pushNotification)
-		{
-			pushNotification = null;
+        /// <summary>
+        /// This method uses an EventGridEvent from EventGrid and tries to create a <see cref="PushNotification"/>
+        /// </summary>
+        /// <param name="eventGridEvent"> EventGridEvent from EventGrid</param>
+        /// <param name="pushNotification"> out parameter set up in this method</param>
+        /// <returns></returns>
+        public static bool TryCreatePushNotification(this EventGridEvent eventGridEvent, out PushNotification pushNotification)
+        {
+            pushNotification = null;
 
-			if (eventGridEvent.Data == null || eventGridEvent.EventType == null || eventGridEvent.Subject == null)
-			{
-				return false;
-			}
-
-			Uri resourceUri;
-
-			if (!Uri.TryCreate(eventGridEvent.Subject, UriKind.Absolute, out resourceUri))
+            if (eventGridEvent.Data == null || eventGridEvent.EventType == null || eventGridEvent.Subject == null)
             {
-				return false;
+                return false;
             }
 
-			try
-			{
-				JsonElement eventGridEventData = JsonDocument.Parse(eventGridEvent.Data.ToString()).RootElement;
+            Uri resourceUri;
 
-				if (eventGridEventData.ValueKind == JsonValueKind.Object &&
-					eventGridEventData.TryGetProperty(SyncTokenPropertyName, out JsonElement syncTokenJson))
+            if (!Uri.TryCreate(eventGridEvent.Subject, UriKind.Absolute, out resourceUri))
+            {
+                return false;
+            }
+
+            try
+            {
+                JsonElement eventGridEventData = JsonDocument.Parse(eventGridEvent.Data.ToString()).RootElement;
+
+                if (eventGridEventData.ValueKind == JsonValueKind.Object &&
+                    eventGridEventData.TryGetProperty(SyncTokenPropertyName, out JsonElement syncTokenJson))
                 {
-					pushNotification = new PushNotification()
-					{
-						SyncToken = syncTokenJson.GetString(),
-						EventType = eventGridEvent.EventType,
-						ResourceUri = resourceUri
-					};
+                    pushNotification = new PushNotification()
+                    {
+                        SyncToken = syncTokenJson.GetString(),
+                        EventType = eventGridEvent.EventType,
+                        ResourceUri = resourceUri
+                    };
 
-					return true;
-				}
+                    return true;
+                }
             }
             catch (JsonException) { }
 
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 }
