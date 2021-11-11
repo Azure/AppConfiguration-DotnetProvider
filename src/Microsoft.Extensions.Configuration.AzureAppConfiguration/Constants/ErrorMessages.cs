e8c4b02ba2484a26c93736d878433c988a197b56
Avani Gupta <avanigupta@users.noreply.github.com>
2021-11-11 13:56:40 -0800

    Validate secret refresh interval

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
@@ -6,5 +6,6 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
     internal class ErrorMessages
     {
         public const string CacheExpirationTimeTooShort = "The cache expiration time cannot be less than {0} milliseconds.";
+        public const string SecretRefreshIntervalTooShort = "The secret refresh interval cannot be less than {0} milliseconds.";
     }
 }
