c363d5b0bdef851c37fd45bd070f928d3011f5f6
Amer Jusupovic <32405726+amerjusupovic@users.noreply.github.com>
2022-11-22 13:08:44 -0600

    Add .net7 as target framework, build with .net7 (#366)
    * change target frameworks, build version in script
    
    * fix comment to net7
    
    * fix missing .net6 install
    
    * fix comment
    
    * test remove version for dotnet install
    
    * revert dotnet install version changes
    
    * update same projects target framework

--- build/install-dotnet.ps1
+++ build/install-dotnet.ps1
@@ -1,6 +1,8 @@
-# Installs .NET 6 for CI/CD environment
+# Installs .NET 6 and .NET 7 for CI/CD environment
 # see: https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script#examples
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Version 7.0.100
 
 &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Version 6.0.301
