548808c37ccf6aeec98c2e11c751e6c44e908dfb
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2022-12-27 11:17:48 -0600

    remove repeated mockconfigclient

--- tests/Tests.AzureAppConfiguration/EnvironmentNameTests.cs
+++ tests/Tests.AzureAppConfiguration/EnvironmentNameTests.cs
@@ -34,7 +34,7 @@ namespace Tests.AzureAppConfiguration
         [Fact]
         public void PreventLoadingKeyValuesWithEnvironmentNameAfterStartup()
         {
-            var mockClient = GetMockConfigurationClient();
+            var mockClient = GetMockConfigurationClientSelectKeyLabel();
             IConfigurationRefresher refresher = null;
             TimeSpan cacheExpirationTime = TimeSpan.FromSeconds(2);
 
@@ -74,7 +74,7 @@ namespace Tests.AzureAppConfiguration
         [Fact]
         public void AllowLoadingKeyValuesWithEnvironmentName()
         {
-            var mockClient = GetMockConfigurationClient();
+            var mockClient = GetMockConfigurationClientSelectKeyLabel();
 
             Environment.SetEnvironmentVariable(RequestTracingConstants.AspNetCoreEnvironmentVariable, "Production");
             Assert.Equal("Production", Environment.GetEnvironmentVariable(RequestTracingConstants.AspNetCoreEnvironmentVariable));
@@ -83,6 +83,8 @@ namespace Tests.AzureAppConfiguration
                 .AddAzureAppConfiguration(environmentName: "Production", options =>
                 {
                     options.Client = mockClient.Object;
+                    options.Select("TestKey1", "label");
+                    options.Select("TestKey2", "label");
                 })
                 .Build();
 
@@ -118,50 +120,6 @@ namespace Tests.AzureAppConfiguration
 
             Assert.Equal("TestValue1", config["TestKey1"]);
             Assert.Null(config["TestKey2"]);
-        }
-
-        private Mock<ConfigurationClient> GetMockConfigurationClient()
-        {
-            var mockResponse = new Mock<Response>();
-            var mockClient = new Mock<ConfigurationClient>(MockBehavior.Strict, TestHelpers.CreateMockEndpointString());
-
-            Response<ConfigurationSetting> GetTestKey(string key, string label, CancellationToken cancellationToken)
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-                }
-
-                return Response.FromValue(TestHelpers.CloneSetting(_kvCollection.FirstOrDefault(s => s.Key == key && s.Label == label)), mockResponse.Object);
-            }
-
-            Response<ConfigurationSetting> GetIfChanged(ConfigurationSetting setting, bool onlyIfChanged, CancellationToken cancellationToken)
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-                }
-
-                var newSetting = _kvCollection.FirstOrDefault(s => (s.Key == setting.Key && s.Label == setting.Label));
-                var unchanged = (newSetting.Key == setting.Key && newSetting.Label == setting.Label && newSetting.Value == setting.Value);
-                var response = new MockResponse(unchanged ? 304 : 200);
-                return Response.FromValue(newSetting, response);
-            }
-
-            // We don't actually select KV based on SettingSelector, we just return a deep copy of _kvCollection
-            mockClient.Setup(c => c.GetConfigurationSettingsAsync(It.IsAny<SettingSelector>(), It.IsAny<CancellationToken>()))
-                .Returns(() =>
-                {
-                    return new MockAsyncPageable(_kvCollection.Select(setting => TestHelpers.CloneSetting(setting)).ToList());
-                });
-
-            mockClient.Setup(c => c.GetConfigurationSettingAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync((Func<string, string, CancellationToken, Response<ConfigurationSetting>>)GetTestKey);
-
-            mockClient.Setup(c => c.GetConfigurationSettingAsync(It.IsAny<ConfigurationSetting>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync((Func<ConfigurationSetting, bool, CancellationToken, Response<ConfigurationSetting>>)GetIfChanged);
-
-            return mockClient;
         }
 
         private Mock<ConfigurationClient> GetMockConfigurationClientSelectKeyLabel()
