9a87c7dae9e4438c6105f97a2212cf8ad8e78b3f
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2023-06-30 15:33:27 -0500

    change emptyconfigrefreshprovider back to internal

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/EmptyConfigurationRefresherProvider.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/EmptyConfigurationRefresherProvider.cs
@@ -5,14 +5,8 @@ using System.Collections.Generic;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 {
-    /// <summary>
-    /// A dummy class used to show that <see cref="AzureAppConfigurationRefresherProvider"/> is not available.
-    /// </summary>
-    public class EmptyConfigurationRefresherProvider : IConfigurationRefresherProvider
+    internal class EmptyConfigurationRefresherProvider : IConfigurationRefresherProvider
     {
-        /// <summary>
-        /// A dummy variable to implement the interface <see cref="IConfigurationRefresherProvider"/>.
-        /// </summary>
         public IEnumerable<IConfigurationRefresher> Refreshers => new List<IConfigurationRefresher>();
     }
 }
