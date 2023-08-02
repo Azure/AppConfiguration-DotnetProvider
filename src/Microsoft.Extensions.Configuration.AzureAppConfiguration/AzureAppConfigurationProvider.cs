e2a6f0be873509849fd35961047c0d8e0e5096ec
Richard Chen <junbchen@microsoft.com>
2023-08-02 18:07:07 +0800

    Not use generic method

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationProvider.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationProvider.cs
@@ -1051,7 +1051,7 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
             }, cancellationToken).ConfigureAwait(false);
         }
 
-        private bool IsFailOverable<TException>(TException ex) where TException : Exception
+        private bool IsFailOverable(Exception ex)
         {
             RequestFailedException rfe = ex.InnerExceptions?.LastOrDefault(e => e is RequestFailedException) as RequestFailedException;
 
