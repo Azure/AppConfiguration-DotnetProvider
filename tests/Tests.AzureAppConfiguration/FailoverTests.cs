2a38215ae0e8800078ee7ab2926542ce833e277d
Richard Chen <junbchen@microsoft.com>
2023-09-12 15:47:06 +0800

    Fix the tess

--- tests/Tests.AzureAppConfiguration/FailoverTests.cs
+++ tests/Tests.AzureAppConfiguration/FailoverTests.cs
@@ -122,7 +122,7 @@ namespace Tests.AzureAppConfiguration
                         refreshOptions.Register("TestKey1", "label")
                             .SetCacheExpiration(TimeSpan.FromSeconds(1));
                     });
-
+                    options.IsAutoFailover = false;
                     refresher = options.GetRefresher();
                 });
 
