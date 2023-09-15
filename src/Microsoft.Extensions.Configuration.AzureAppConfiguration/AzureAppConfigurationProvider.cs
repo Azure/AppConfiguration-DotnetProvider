bc603294a30a47a86fc3f3b8f4fef5b9039c5c6e
Richard Chen <junbchen@microsoft.com>
2023-09-15 22:11:13 +0800

    Stop once it has been auto failovered

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationProvider.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationProvider.cs
@@ -955,6 +955,8 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
                 Uri previousEndpoint = _configClientManager.GetEndpointForClient(clientEnumerator.Current);
                 ConfigurationClient currentClient;
 
+                bool autoFailovered = false;
+
                 while (true)
                 {
                     bool success = false;
@@ -980,9 +982,10 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
                         }
                         else if (!clientEnumerator.MoveNext())
                         {
-                            if (_options.IsAutoFailover)
+                            if (_options.IsAutoFailover && !autoFailovered)
                             {
                                 IEnumerable<ConfigurationClient> autoFailoverClients = await _configClientManager.GetAutoFailoverClients(_logger, cancellationToken).ConfigureAwait(false);
+                                autoFailovered = true;
 
                                 _logger.LogDebug(LogHelper.BuildAutoFailoverClientCountMessage(autoFailoverClients?.Count() ?? 0));
 
