c06849271676fe153fb75aa6892cfedbd1fe1977
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2024-02-14 11:43:33 -0800

    fix extension

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Extensions/BytesExtensions.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Extensions/BytesExtensions.cs
@@ -10,31 +10,31 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Extensions
     {
         public static string ToBase64Url(this byte[] bytes)
         {
-            string featureFlagIdBase64 = Convert.ToBase64String(bytes);
+            string bytesBase64 = Convert.ToBase64String(bytes);
 
-            int indexOfEquals = featureFlagIdBase64.IndexOf("=");
+            int indexOfEquals = bytesBase64.IndexOf("=");
 
-            int stringBuilderCapacity = indexOfEquals != -1 ? indexOfEquals : featureFlagIdBase64.Length;
+            int stringBuilderCapacity = indexOfEquals != -1 ? indexOfEquals : bytesBase64.Length;
 
-            StringBuilder featureFlagIdBuilder = new StringBuilder(stringBuilderCapacity);
+            StringBuilder stringBuilder = new StringBuilder(stringBuilderCapacity);
 
             for (int i = 0; i < stringBuilderCapacity; i++)
             {
-                if (featureFlagIdBase64[i] == '+')
+                if (bytesBase64[i] == '+')
                 {
-                    featureFlagIdBuilder.Append('-');
+                    stringBuilder.Append('-');
                 }
-                else if (featureFlagIdBase64[i] == '/')
+                else if (bytesBase64[i] == '/')
                 {
-                    featureFlagIdBuilder.Append('_');
+                    stringBuilder.Append('_');
                 }
                 else
                 {
-                    featureFlagIdBuilder.Append(featureFlagIdBase64[i]);
+                    stringBuilder.Append(bytesBase64[i]);
                 }
             }
 
-            return featureFlagIdBuilder.ToString();
+            return stringBuilder.ToString();
         }
     }
 }
