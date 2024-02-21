4181b991eca3c7e4be7ccb76a314c4ea25dea711
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2024-02-21 12:26:36 -0800

    fix tests backoff duration

--- tests/Tests.AzureAppConfiguration/RefreshTests.cs
+++ tests/Tests.AzureAppConfiguration/RefreshTests.cs
@@ -730,7 +730,7 @@ namespace Tests.AzureAppConfiguration
             .AddAzureAppConfiguration(options =>
             {
                 options.Select("TestKey*");
-                options.MinBackoffDuration = TimeSpan.FromMilliseconds(1);
+                options.MinBackoffDuration = TimeSpan.FromTicks(1);
                 options.ClientManager = TestHelpers.CreateMockedConfigurationClientManager(mockClient.Object);
                 options.ConfigureRefresh(refreshOptions =>
                 {
@@ -802,7 +802,7 @@ namespace Tests.AzureAppConfiguration
             var config = new ConfigurationBuilder()
                 .AddAzureAppConfiguration(options =>
                 {
-                    options.MinBackoffDuration = TimeSpan.FromMilliseconds(1);
+                    options.MinBackoffDuration = TimeSpan.FromTicks(1);
                     options.ClientManager = TestHelpers.CreateMockedConfigurationClientManager(mockClient.Object);
                     options.Select("TestKey*", "label");
                     options.ConfigureRefresh(refreshOptions =>
