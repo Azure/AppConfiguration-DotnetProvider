61b160be334050585c4e770dcaf23e4e25504cc1
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2024-03-27 13:46:21 -0700

    edit error message

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
@@ -7,6 +7,6 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
     {
         public const string CacheExpirationTimeTooShort = "The cache expiration time cannot be less than {0} milliseconds.";
         public const string SecretRefreshIntervalTooShort = "The secret refresh interval cannot be less than {0} milliseconds.";
-        public const string FeatureFlagInvalidJsonProperty = "Invalid property '{0}' for feature flag '{1}'. Found type '{2}', but expected type '{3}'.";
+        public const string FeatureFlagInvalidJsonProperty = "Invalid property '{0}' for feature flag '{1}'. Found type: '{2}'. Expected type: '{3}'.";
     }
 }
