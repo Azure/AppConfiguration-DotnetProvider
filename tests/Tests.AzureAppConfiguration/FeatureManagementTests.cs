b8a2b150183903811db2e37cb9be08de3cedb16f
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2024-05-31 13:48:06 -0700

    remove unused using

--- tests/Tests.AzureAppConfiguration/FeatureManagementTests.cs
+++ tests/Tests.AzureAppConfiguration/FeatureManagementTests.cs
@@ -15,7 +15,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
