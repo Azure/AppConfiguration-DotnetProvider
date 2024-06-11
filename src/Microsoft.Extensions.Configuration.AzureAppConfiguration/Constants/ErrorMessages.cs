63dc59cb23b817b07cdcfebd4f36334c6288a2e3
Amer Jusupovic <32405726+amerjusupovic@users.noreply.github.com>
2024-06-11 10:15:43 -0700

    Update src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
    Co-authored-by: Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/ErrorMessages.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
         public const string RefreshIntervalTooShort = "The refresh interval cannot be less than {0} milliseconds.";
         public const string SecretRefreshIntervalTooShort = "The secret refresh interval cannot be less than {0} milliseconds.";
         public const string FeatureFlagInvalidJsonProperty = "Invalid property '{0}' for feature flag. Key: '{1}'. Found type: '{2}'. Expected type: '{3}'.";
-        public const string FeatureFlagInvalidFormat = "Invalid json format for feature flag. Key: '{0}'";
+        public const string FeatureFlagInvalidFormat = "Invalid json format for feature flag. Key: '{0}'.";
         public const string InvalidKeyVaultReference = "Invalid Key Vault reference.";
     }
 }
