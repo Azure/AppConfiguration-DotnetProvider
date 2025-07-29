02ac2d2d42afbb1cf7e9f40aa5327ff567a57aba
Vaishnavi Vidyasagar <t-vvidyasaga@microsoft.com>
2025-07-29 14:16:45 -0700

    feat: defined snapshot reference content type and Json Property

---
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/Constants/SnapshotReferenceConstants.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
+{
+    internal class SnapshotReferenceConstants
+    {
+        public const string ContentType = "application/json; profile=\"https://azconfig.io/mime-profiles/snapshot-ref+json\"";
+
+        public const string SnapshotReferenceJsonPropertyName = "snapshot_name";
+    }
+}
