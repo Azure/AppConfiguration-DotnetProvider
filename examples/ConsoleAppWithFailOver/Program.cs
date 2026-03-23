fb8a088ec40d7b19b3d97fb60b6f341094c127a9
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2026-03-23 17:20:40 +0800

    add null check in examples

--- examples/ConsoleAppWithFailOver/Program.cs
+++ examples/ConsoleAppWithFailOver/Program.cs
@@ -31,7 +31,10 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.Cons
             IConfiguration configuration = builder.Build();
 
             IConfigurationSection endpointsSection = configuration.GetSection("AppConfig:Endpoints");
-            IEnumerable<Uri> endpoints = endpointsSection.GetChildren().Select(endpoint => new Uri(endpoint.Value));
+            IEnumerable<Uri> endpoints = endpointsSection.GetChildren()
+                .Select(endpoint => endpoint.Value)
+                .Where(value => !string.IsNullOrEmpty(value))
+                .Select(value => new Uri(value));
 
             if (endpoints == null || !endpoints.Any())
             {
