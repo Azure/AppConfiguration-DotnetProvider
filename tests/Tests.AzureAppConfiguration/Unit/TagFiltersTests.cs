5ccb0b0bc2352471b09c52bd90464927cd3ef694
Abhinav Jain <iamabjain@gmail.com>
2026-04-18 21:53:45 +0530

    Update TagFiltersTests.cs

--- tests/Tests.AzureAppConfiguration/Unit/TagFiltersTests.cs
+++ tests/Tests.AzureAppConfiguration/Unit/TagFiltersTests.cs
@@ -159,6 +159,30 @@ namespace Tests.AzureAppConfiguration
         }
 
         [Fact]
+        public void KeyValueWatcher_EqualityAndHash_IncludesTags()
+        {
+            var tags = new List<string> { "v1", "v2" };
+        
+            var watcher1 = new KeyValueWatcher { Key = "TestKey", Tags = tags };
+            var watcher2 = new KeyValueWatcher { Key = "TestKey", Tags = new List<string> { "v2", "v1" } }; // Different order
+        
+            // Test Equality (should be true if you updated Equals to use SetEquals)
+            Assert.Equal(watcher1, watcher2);
+        
+            // Test HashCode
+            Assert.Equal(watcher1.GetHashCode(), watcher2.GetHashCode());
+        
+            var watcher3 = new KeyValueWatcher { Key = "TestKey", Tags = tags };
+            var watcher4 = new KeyValueWatcher { Key = "TestKey", Tags = new List<string> { "v2", "v1", "v3" } }; // Different tags
+        
+            // Test Equality (should be true if you updated Equals to use SetEquals)
+            Assert.NotEqual(watcher3, watcher4);
+        
+            // Test HashCode
+            Assert.NotEqual(watcher3.GetHashCode(), watcher4.GetHashCode());
+        }
+
+        [Fact]
         public void TagFiltersTests_BasicTagFiltering()
         {
             var mockClient = new Mock<ConfigurationClient>(MockBehavior.Strict);
