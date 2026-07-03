0b9a99072b2af86a77f0742b1cc65d9a4286f346
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2026-07-03 10:16:27 +0800

    update

---
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/FeatureManagement/FeatureGroupAllocation.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.FeatureManagement
+{
+    internal class FeatureGroupAllocation
+    {
+        public string Variant { get; set; }
+
+        public IEnumerable<string> Groups { get; set; }
+    }
+}
---
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/FeatureManagement/FeatureVariant.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+using System.Text.Json;
+
+namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.FeatureManagement
+{
+    internal class FeatureVariant
+    {
+        public string Name { get; set; }
+
+        public JsonElement ConfigurationValue { get; set; }
+
+        public string StatusOverride { get; set; }
+    }
+}
